83d1d59bcc94bdea758c4c20accdbb2e35a958a1
Jiayi Chen <jiayi@gatech.edu>
2025-04-18 22:01:31 -0400

    Support Runtime Creation Using GUI
    Change-Id: I98167d0bd2249c253dc38505bf7277099b5e29bc

--- source/reflect/reflect.hpp
+++ source/reflect/reflect.hpp
@@ -61,43 +61,64 @@ namespace Vkxel {
         Reflect() = delete;
         ~Reflect() = delete;
 
+        using ID = entt::id_type;
+        using Type = entt::meta_type;
+
         template<typename InternalType>
         static void RegisterType() {
             static_assert(requires { InternalType::Register; }, "Type Not Registered");
-            using Type = typename InternalType::Type;
-            if (!_type_map.contains(typeid(Type))) {
+            using TargetType = typename InternalType::Type;
+            if (!_type_map.contains(typeid(TargetType))) {
                 InternalType::Register();
-                _type_map[typeid(Type)] = entt::resolve<Type>();
+                auto type = GetType<TargetType>();
+                std::type_index index = typeid(TargetType);
+                _type_map[index] = type;
+                for (auto &&[id, base]: type.base()) {
+                    _derived_map[id].push_back(type);
+                }
             }
         }
 
         template<typename T>
-        static entt::meta_type GetType() {
+        static Type GetType() {
             return entt::resolve<T>();
         }
 
-        static const entt::meta_type &GetType(const std::type_index &type) {
+        static const Type &GetType(const std::type_index &type) {
             CHECK(_type_map.contains(type), "Type Not Registered: {}", type.name());
             return _type_map.at(type);
         }
 
-        static const std::optional<entt::meta_type> &TryGetType(const std::type_index &type) {
+        static const std::optional<Type> &TryGetType(const std::type_index &type) {
             if (_type_map.contains(type)) {
                 return _type_map.at(type);
             }
             return std::nullopt;
         }
 
-        static void SetName(const entt::id_type id, const std::string_view name) { _name_map[id] = name; }
+        static const std::vector<Type> &GetDerived(const Type &type) {
+            CHECK(_derived_map.contains(type.id()), "Derived Class Not Registered: {}", Reflect::GetName(type.id()));
+            return _derived_map.at(type.id());
+        }
 
-        static std::string_view GetName(const entt::id_type id) {
+        static const std::optional<std::vector<Type>> &TryGetDerived(const Type &type) {
+            if (_derived_map.contains(type.id())) {
+                return _derived_map.at(type.id());
+            }
+            return std::nullopt;
+        }
+
+        static void SetName(const ID id, const std::string_view name) { _name_map[id] = name; }
+
+        static std::string_view GetName(const ID id) {
             CHECK(_name_map.contains(id), "Name Not Registered");
             return _name_map.at(id);
         }
 
     private:
-        inline static std::unordered_map<std::type_index, entt::meta_type> _type_map;
-        inline static std::unordered_map<entt::id_type, std::string_view> _name_map;
+        inline static std::unordered_map<std::type_index, Type> _type_map;
+        inline static std::unordered_map<ID, std::vector<Type>> _derived_map;
+        inline static std::unordered_map<ID, std::string_view> _name_map;
     };
 
 } // namespace Vkxel
